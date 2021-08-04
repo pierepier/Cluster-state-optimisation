fb565cde8d604eaa9560fca7973b1b6fddd6c3f7
pierepier <88136682+pierepier@users.noreply.github.com>
2021-08-04 15:59:10 +0200

    New M
    I have now swapped how the mode coupling matrix is constructed for a more efficient alternative.

--- my code.py
+++ my code.py
@@ -36,182 +36,125 @@ from scipy.optimize import minimize
 import scipy.linalg as LA    
 import matplotlib.pyplot as plt       
 def covariance_differance(pump,n):
-#pump=[0,0,30e3*1e-9,30e3*1e-9,30e3*1e-9,0,0,0,0,0,np.pi,0,0,0]  
     pumpinfo=np.array_split(pump,2)
     pump_amp=pumpinfo[0]
     pump_phase=pumpinfo[1]
     #number of modes in covariance matrix
     pump_length=len(pump_amp)
     n_modes=(pump_length+1)//2
-    #mode index
-    #generates a comb of modes centered on index 0
- #bleh   
-    #the index for the pump positions considered remember the firt and last one 
-    #in this index and mode index are the same "point".
-    
-    n_index = np.arange(pump_length) - pump_length//2
-    
-    #the indexes for modes in the system
-    mode_index=n_index[0::2]//2
+     #mode index
+     #generates a comb of modes centered on index 0 
+     #the index for the pump positions considered remember the firt and last one 
+     #in this index and mode index are the same "point".
+     
+    n_index = np.arange(pump_length)//2
+     
+     #the indexes for modes in the system
+    mode_index=n_index[0::2]
     #--------3 wave mixing -----------------------
-    #pump_frequencies (GHz---->KHz)
-    p1_freq = 2*  2*np.pi*4.3023*1e6
-    p2_freq = 2*  2*np.pi*4.2977*1e6
+    #pump_frequencies (kHz)
+    p1_freq = 2*  2*np.pi*4.3023e6
+    p2_freq = 2*  2*np.pi*4.2977e6
     
     #mode frequencies, set up frequency comb
-    
     center_comb_freq = (p2_freq + p1_freq)/4
     comb_det = np.abs(p2_freq - p1_freq)/4
-    comb_freqs = center_comb_freq + (mode_index)*comb_det
     
-    
-    #--------4 wave mixing -----------------------
-    #pump_frequencies (GHz)
-    # p1_freq = 2*np.pi*4.3023
-    # p2_freq = 2*np.pi*4.2977
-    
-    # #mode frequencies, set up frequency comb
-    # center_comb_freq = (p2_freq + p1_freq)/2
-    # comb_det = np.abs(p2_freq - p1_freq)/2
-    # comb_freqs = center_comb_freq + n_index*comb_det
+    comb_freqs = center_comb_freq + (mode_index - n_modes//2 )*comb_det
     
     
     #-----------set pump positions and strengths -------------------
     #pump mode positions, enter a numpy array
-    
-    pump_pos1=np.where(pump_amp!=0)
-    ps =np.subtract(pump_pos1,pump_length//2)
-    pump_position=ps[0]
-    #center pump amp and phase (enter a complex number)
-    #effective pump amplitude
-    # phi1 = 30e3*np.exp(1j*np.pi)*1e-9
-    # phi2 = 30e3*np.exp(0)*1e-9
-    # phi3 = 30e3*np.exp(0)*1e-9
-       
-    pump_strength =pump_amp[pump_pos1]*np.exp(1j*pump_phase[pump_pos1])
-    # pump_position = np.array([-1])
-    # pump_strength = np.array([phi4])
+    pump_pos=np.asarray(np.where(pump_amp!=0))
+    pump_position=pump_pos[0]
+    #effective pump amplitude  
+    pump_strength = pump_amp[pump_position]*np.exp(1j*pump_phase[pump_position])
     
     #-------------- resonances and losses --------------------------
     
-    # #JPA resonance (GHz)
-    # omega_0p = np.array([ 2*np.pi*4.3*10**9 /10**9 ])
+    # #JPA resonance (kHz)
+    omega_0 = np.array([ 2*np.pi*4.3e6])
     
-    #SAW resonances (GHz------>KHz)
-    omega_0p = comb_freqs + 3e3*1e6/1e9*2*np.pi
+    #SAW resonances (kHz)
+    #omega_0p = comb_freqs + 3*2*np.pi
     
-    #dissipation rates (GHZ------->KHz)
-    gamma_int = 2*np.pi*20e3*1e6/1e9 * np.ones_like(omega_0p)
-    #leave at 0, calculation does not take internal noise channels into account
-    # gamma_ext = 2*np.pi*300e6 /1e9 * np.ones_like(omega_0p)
-    gamma_ext = 2*np.pi*20e3*1e6/1e9 * np.ones_like(omega_0p)
-    gamma_p = gamma_ext + gamma_int
+    #dissipation rates (kHZ)
+    gamma = 2*np.pi*20
     
     
-    
+    #bias point
+    phi_DC = 0.6*np.pi
     
     #------generate scattering matrix--------------
-    #using only up to first order 
+    #using only up to first order
     #mode matrix
     M = np.zeros(( n_modes*2, n_modes*2 ), dtype = complex)
     
-    for i, s in enumerate(mode_index):
-        
-        idler_positions = (2*pump_position) - s
-        
-        if len(omega_0p) == 1:
-            omega_0 = omega_0p[0]
-            gamma = gamma_p[0]
-            K = np.identity(2*n_modes)*np.sqrt(gamma_ext)
-            K_loss = np.identity(2*n_modes)*np.sqrt(gamma_int)
-        else:
-            omega_0 = omega_0p[i]
-            gamma = gamma_p[i]
-            K = np.block( [[ np.diag(np.sqrt(gamma_ext)), np.zeros((n_modes, n_modes)) ], 
-                            [ np.zeros((n_modes, n_modes)), np.diag(np.sqrt(gamma_ext)) ]] )
-            K_loss = np.block( [[ np.diag(np.sqrt(gamma_int)), np.zeros((n_modes, n_modes)) ], 
-                                [ np.zeros((n_modes, n_modes)), np.diag(np.sqrt(gamma_int)) ]] )
+    print("mode index", mode_index)
+    print("pump_position", pump_position)
     
-        
+    for s in mode_index: # mode index is "regular array"
+    
+        idler_positions = pump_position - s
+        print("idler pos", idler_positions, "n_idx", s)
+    
         #constructing the mode coupling matrix M
-            for pos, i1 in enumerate(idler_positions):
-                if np.any(mode_index == i1):
-                    j = np.where(mode_index == i1)[0][0]
-                    #coupling strength given below
-                    #coupling depends on system, for SAW see the off-diagonal terms below eq. S35 in SAW supplementary
-                    #should be much smaller than the linewidth
-                    # conver_rate = -omega_0/4*np.tan(phi_DC)*pump_strength[pos]*np.pi #for JPA
-                    conver_rate = -pump_strength[pos]
-                    M[i,j+n_modes] = conver_rate
-                    M[i+n_modes, j] = -np.conjugate(conver_rate)
-        
+        for pos, i1 in enumerate(idler_positions):
+            #print("POS",pos, i1)
+            if np.any(mode_index == i1) and s != i1:
+                print("n %d, pos %d, i1 %d"%(s, pos, i1))
+                #coupling strength given below
+                conver_rate = -pump_strength[pos]
+                M[s,i1+n_modes] = conver_rate
+                M[s+n_modes, i1] = -np.conjugate(conver_rate)
     
-        diag_val = comb_freqs[i] - omega_0 + 1j*gamma/2
+        diag_val = comb_freqs[n] - omega_0 + 1j*gamma/2
         diag_val_conj = -np.conjugate(diag_val)
-       
-        M[i, i] = diag_val
-        M[i+n_modes, i+n_modes] = diag_val_conj
-        
-        #figure 1   
-        # # fig, ax = plt.subplots(figsize=(8,8))
-        # # ax.imshow(np.real(M[::2,::2]))
-        
-        # grid_arr = np.arange(0, 2*n_modes, 1)
-        # plt.xticks( grid_arr)
-        # plt.yticks( grid_arr)
-        # # ax.set_xticks(grid_arr, minor=True)
-        # # ax.set_yticks(grid_arr, minor=True)
-        # ax.grid(True, which='major', axis='both', linestyle='-', color='w', linewidth=0.5)
-        # # K = np.identity(2*n_modes)*np.sqrt(gamma_ext)
-        # ax.set_xticklabels(np.concatenate((mode_index,mode_index)))
-        # ax.set_yticklabels(np.concatenate((mode_index,mode_index)))
-        # #calculating the scattering matrix
-        
+    
+        M[s, s] = diag_val
+        M[s+n_modes, s+n_modes] = diag_val_conj
+    
+    #print(np.real(M[:n_modes,n_modes:]))
+    #exit()
+    print(M)
+    
+    K = np.identity(2*n_modes)*np.sqrt(gamma)
+    
+    #calculating the scattering matrix
     M_inv = np.linalg.inv(M)
     S = 1j*K.dot(M_inv).dot(K) - np.identity(2*n_modes)
-    #for the internal loss channel, we need a second term
-    Sp = 1j*K.dot(M_inv).dot(K_loss)
-        
-        
-    #plot gain across the frequency comb
-    # gain_curve = np.abs(np.diag(S))[:n_modes]
-    #figure 2
-    # fig, ax = plt.subplots(1)
-    # ax.plot( comb_freqs/(2*np.pi), 20*np.log10(gain_curve) )
-        
+    
     # ----------------------- calculate the covariance matrix ----------------
-        #convert scattering basis to the x, p basis
-        #the vector ordering becomes (x1, x2, ...., p1,p2,....)
+    #convert scattering basis to the x, p basis
+    #the vector ordering becomes (x1, x2, ...., p1,p2,....)
     X = np.zeros_like(S, dtype = complex)
     for ii in range(n_modes):
-            X[ii, ii], X[ii, ii+n_modes] = np.ones(2)
-            X[ii+n_modes, ii] = -1j
-            X[ii+n_modes, ii+n_modes] = 1j
-            
-        #transform scattering matrix
+        X[ii, ii], X[ii, ii+n_modes] = np.ones(2)
+        X[ii+n_modes, ii] = -1j
+        X[ii+n_modes, ii+n_modes] = 1j
+    
+    #transform scattering matrix
     X_inv = np.linalg.inv(X)
     S_xp = X.dot(S).dot(X_inv)
-    Sp_xp = X.dot(Sp).dot(X_inv)
-        
-        
-        #check if it is symplectic
+    
+    #check if it is symplectic
     symp = np.block( [ [np.zeros((n_modes, n_modes)), np.identity(n_modes)], [-1*np.identity(n_modes), np.zeros((n_modes, n_modes))] ] )
-        
+    
     symp2 = S_xp.dot(symp).dot(np.transpose(S_xp))
-        
+    
     result = np.all(np.round( np.real(symp2) , 3) == symp )
     print(' The transformation is symplectic: ' + str(result) )
-        
-        # calculate resulting covariance matrix
-        #input noise:
+    
+    # calculate resulting covariance matrix
+    #input noise:
     n_noise = 0
     V_input = (2*n_noise + 1)*np.identity(2*n_modes)
     #noise from loss channel, we can assume it is at the same temperature as the input channel
     V_loss = V_input
     #output statistics
-    noise_from_loss_channel = np.real(Sp_xp.dot(V_loss).dot( np.transpose(Sp_xp) ))
-    V_output = np.real(S_xp.dot(V_input).dot( np.transpose(S_xp) )) + noise_from_loss_channel
-    #calculate the theoretical covariance matrix
+    
+    V_output = np.real(S_xp.dot(V_input).dot( np.transpose(S_xp) ))
+    
     G=nx.grid_2d_graph(n, n, periodic=False, create_using=None) 
     adj=nx.to_numpy_matrix(G)
     # $U=(I+iV)(V^2+I)^{-1/2}=AB^{-1/2}
@@ -233,182 +176,125 @@ def covariance_differance(pump,n):
     return V_diff
 
 def covariances(pump,n):
-#pump=[0,0,30e3*1e-9,30e3*1e-9,30e3*1e-9,0,0,0,0,0,np.pi,0,0,0]  
     pumpinfo=np.array_split(pump,2)
     pump_amp=pumpinfo[0]
     pump_phase=pumpinfo[1]
     #number of modes in covariance matrix
     pump_length=len(pump_amp)
     n_modes=(pump_length+1)//2
-    #mode index
-    #generates a comb of modes centered on index 0
-    
-    #the index for the pump positions considered remember the firt and last one 
-    #in this index and mode index are the same "point".
-    
-    n_index = np.arange(pump_length) - pump_length//2
-    
-    #the indexes for modes in the system
+     #mode index
+     #generates a comb of modes centered on index 0 
+     #the index for the pump positions considered remember the firt and last one 
+     #in this index and mode index are the same "point".
+     
+    n_index = np.arange(pump_length)//2
+     
+     #the indexes for modes in the system
     mode_index=n_index[0::2]
     #--------3 wave mixing -----------------------
-    #pump_frequencies (GHz----->KHz)
-    p1_freq = 2*  2*np.pi*4.3023*1e6
-    p2_freq = 2*  2*np.pi*4.2977*1e6
+    #pump_frequencies (kHz)
+    p1_freq = 2*  2*np.pi*4.3023e6
+    p2_freq = 2*  2*np.pi*4.2977e6
     
     #mode frequencies, set up frequency comb
-    
     center_comb_freq = (p2_freq + p1_freq)/4
     comb_det = np.abs(p2_freq - p1_freq)/4
-    comb_freqs = center_comb_freq + (mode_index//2)*comb_det
     
-    
-    #--------4 wave mixing -----------------------
-    #pump_frequencies (GHz)
-    # p1_freq = 2*np.pi*4.3023
-    # p2_freq = 2*np.pi*4.2977
-    
-    # #mode frequencies, set up frequency comb
-    # center_comb_freq = (p2_freq + p1_freq)/2
-    # comb_det = np.abs(p2_freq - p1_freq)/2
-    # comb_freqs = center_comb_freq + n_index*comb_det
+    comb_freqs = center_comb_freq + (mode_index - n_modes//2 )*comb_det
     
     
     #-----------set pump positions and strengths -------------------
     #pump mode positions, enter a numpy array
-    
-    pump_pos1=np.where(pump_amp!=0)
-    ps =np.subtract(pump_pos1,pump_length//2)
-    pump_position=ps[0]
-    #enter pump amp and phase (enter a complex number)
-    #effective pump amplitude
-    # phi1 = 30e3*np.exp(1j*np.pi)*1e-9
-    # phi2 = 30e3*np.exp(0)*1e-9
-    # phi3 = 30e3*np.exp(0)*1e-9
-       
-    pump_strength = pump_amp[pump_pos1]*np.exp(1j*pump_phase[pump_pos1])
-    # pump_position = np.array([-1])
-    # pump_strength = np.array([phi4])
+    pump_pos=np.asarray(np.where(pump_amp!=0))
+    pump_position=pump_pos[0]
+    #effective pump amplitude  
+    pump_strength = pump_amp[pump_position]*np.exp(1j*pump_phase[pump_position])
     
     #-------------- resonances and losses --------------------------
     
-    # #JPA resonance (GHz)
-    # omega_0p = np.array([ 2*np.pi*4.3*10**9 /10**9 ])
+    # #JPA resonance (kHz)
+    omega_0 = np.array([ 2*np.pi*4.3e6])
     
-    #SAW resonances (GHz------>KHz)
-    omega_0p = comb_freqs + 3e3*1e6/1e9*2*np.pi
+    #SAW resonances (kHz)
+    #omega_0p = comb_freqs + 3*2*np.pi
     
-    #dissipation rates (GHZ------->KHz)
-    gamma_int = 2*np.pi*20e3*1e6/1e9 * np.ones_like(omega_0p)
-    #leave at 0, calculation does not take internal noise channels into account
-    # gamma_ext = 2*np.pi*300e6 /1e9 * np.ones_like(omega_0p)
-    gamma_ext = 2*np.pi*20e3*1e6/1e9 * np.ones_like(omega_0p)
-    gamma_p = gamma_ext + gamma_int
+    #dissipation rates (kHZ)
+    gamma = 2*np.pi*20
     
     
-    
+    #bias point
+    phi_DC = 0.6*np.pi
     
     #------generate scattering matrix--------------
-    #using only up to first order 
+    #using only up to first order
     #mode matrix
     M = np.zeros(( n_modes*2, n_modes*2 ), dtype = complex)
     
-    for i, s in enumerate(mode_index):
-        
-        idler_positions = (2*pump_position) - s
-        
-        if len(omega_0p) == 1:
-            omega_0 = omega_0p[0]
-            gamma = gamma_p[0]
-            K = np.identity(2*n_modes)*np.sqrt(gamma_ext)
-            K_loss = np.identity(2*n_modes)*np.sqrt(gamma_int)
-        else:
-            omega_0 = omega_0p[i]
-            gamma = gamma_p[i]
-            K = np.block( [[ np.diag(np.sqrt(gamma_ext)), np.zeros((n_modes, n_modes)) ], 
-                            [ np.zeros((n_modes, n_modes)), np.diag(np.sqrt(gamma_ext)) ]] )
-            K_loss = np.block( [[ np.diag(np.sqrt(gamma_int)), np.zeros((n_modes, n_modes)) ], 
-                                [ np.zeros((n_modes, n_modes)), np.diag(np.sqrt(gamma_int)) ]] )
+    print("mode index", mode_index)
+    print("pump_position", pump_position)
     
-        
+    for s in mode_index: # mode index is "regular array"
+    
+        idler_positions = pump_position - s
+        print("idler pos", idler_positions, "n_idx", s)
+    
         #constructing the mode coupling matrix M
-            for pos, i1 in enumerate(idler_positions):
-                if np.any(mode_index == i1):
-                    j = np.where(mode_index == i1)[0][0]
-                    #coupling strength given below
-                    #coupling depends on system, for SAW see the off-diagonal terms below eq. S35 in SAW supplementary
-                    #should be much smaller than the linewidth
-                    # conver_rate = -omega_0/4*np.tan(phi_DC)*pump_strength[pos]*np.pi #for JPA
-                    conver_rate = -pump_strength[pos]
-                    M[i,j+n_modes] = conver_rate
-                    M[i+n_modes, j] = -np.conjugate(conver_rate)
-        
+        for pos, i1 in enumerate(idler_positions):
+            #print("POS",pos, i1)
+            if np.any(mode_index == i1) and s != i1:
+                print("n %d, pos %d, i1 %d"%(s, pos, i1))
+                #coupling strength given below
+                conver_rate = -pump_strength[pos]
+                M[s,i1+n_modes] = conver_rate
+                M[s+n_modes, i1] = -np.conjugate(conver_rate)
     
-        diag_val = comb_freqs[i] - omega_0 + 1j*gamma/2
+        diag_val = comb_freqs[s] - omega_0 + 1j*gamma/2
         diag_val_conj = -np.conjugate(diag_val)
-       
-        M[i, i] = diag_val
-        M[i+n_modes, i+n_modes] = diag_val_conj
-        
-        #figure 1   
-        # # fig, ax = plt.subplots(figsize=(8,8))
-        # # ax.imshow(np.real(M[::2,::2]))
-        
-        # grid_arr = np.arange(0, 2*n_modes, 1)
-        # plt.xticks( grid_arr)
-        # plt.yticks( grid_arr)
-        # # ax.set_xticks(grid_arr, minor=True)
-        # # ax.set_yticks(grid_arr, minor=True)
-        # ax.grid(True, which='major', axis='both', linestyle='-', color='w', linewidth=0.5)
-        # # K = np.identity(2*n_modes)*np.sqrt(gamma_ext)
-        # ax.set_xticklabels(np.concatenate((mode_index,mode_index)))
-        # ax.set_yticklabels(np.concatenate((mode_index,mode_index)))
-        # #calculating the scattering matrix
-        
+    
+        M[s, s] = diag_val
+        M[s+n_modes, s+n_modes] = diag_val_conj
+    
+    #print(np.real(M[:n_modes,n_modes:]))
+    #exit()
+    print(M)
+    
+    K = np.identity(2*n_modes)*np.sqrt(gamma)
+    
+    #calculating the scattering matrix
     M_inv = np.linalg.inv(M)
     S = 1j*K.dot(M_inv).dot(K) - np.identity(2*n_modes)
-    #for the internal loss channel, we need a second term
-    Sp = 1j*K.dot(M_inv).dot(K_loss)
-        
-        
-    #plot gain across the frequency comb
-    # gain_curve = np.abs(np.diag(S))[:n_modes]
-    #figure 2
-    # fig, ax = plt.subplots(1)
-    # ax.plot( comb_freqs/(2*np.pi), 20*np.log10(gain_curve) )
-        
+    
     # ----------------------- calculate the covariance matrix ----------------
-        #convert scattering basis to the x, p basis
-        #the vector ordering becomes (x1, x2, ...., p1,p2,....)
+    #convert scattering basis to the x, p basis
+    #the vector ordering becomes (x1, x2, ...., p1,p2,....)
     X = np.zeros_like(S, dtype = complex)
     for ii in range(n_modes):
-            X[ii, ii], X[ii, ii+n_modes] = np.ones(2)
-            X[ii+n_modes, ii] = -1j
-            X[ii+n_modes, ii+n_modes] = 1j
-            
-        #transform scattering matrix
+        X[ii, ii], X[ii, ii+n_modes] = np.ones(2)
+        X[ii+n_modes, ii] = -1j
+        X[ii+n_modes, ii+n_modes] = 1j
+    
+    #transform scattering matrix
     X_inv = np.linalg.inv(X)
     S_xp = X.dot(S).dot(X_inv)
-    Sp_xp = X.dot(Sp).dot(X_inv)
-        
-        
-        #check if it is symplectic
+    
+    #check if it is symplectic
     symp = np.block( [ [np.zeros((n_modes, n_modes)), np.identity(n_modes)], [-1*np.identity(n_modes), np.zeros((n_modes, n_modes))] ] )
-        
+    
     symp2 = S_xp.dot(symp).dot(np.transpose(S_xp))
-        
+    
     result = np.all(np.round( np.real(symp2) , 3) == symp )
     print(' The transformation is symplectic: ' + str(result) )
-        
-        # calculate resulting covariance matrix
-        #input noise:
+    
+    # calculate resulting covariance matrix
+    #input noise:
     n_noise = 0
     V_input = (2*n_noise + 1)*np.identity(2*n_modes)
     #noise from loss channel, we can assume it is at the same temperature as the input channel
     V_loss = V_input
     #output statistics
-    noise_from_loss_channel = np.real(Sp_xp.dot(V_loss).dot( np.transpose(Sp_xp) ))
-    V_output = np.real(S_xp.dot(V_input).dot( np.transpose(S_xp) )) + noise_from_loss_channel
-    #calculate the theoretical covariance matrix
+    
+    V_output = np.real(S_xp.dot(V_input).dot( np.transpose(S_xp) ))
+    
     G=nx.grid_2d_graph(n, n, periodic=False, create_using=None) 
     adj=nx.to_numpy_matrix(G)
     # $U=(I+iV)(V^2+I)^{-1/2}=AB^{-1/2}
@@ -444,7 +330,7 @@ out=minimize(covariance_differance,pump,n,method = 'Nelder-Mead',bounds=boundary
 pump1=out.x
 pumpamp=pump1[:b1]
 pumpphi=pump1[b1:]
-range1=np.arange(b1)-(b1//2)
+range1=np.arange(b1)
 plt.bar(range1,pumpamp)
 plt.ylabel("The amplitude")
 plt.xlabel("The pump index")
